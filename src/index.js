67e2ab18ad2cdd085cfed93fb617fc72989170cd
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-03-31 20:08:36 +0430

    done list structure of todo list

--- src/index.js
+++ src/index.js
@@ -1,20 +1,45 @@
-import _ from 'lodash';
 import './style.css';
-import Icon from './icon.png';
-function component() {
-  const element = document.createElement('div');
+import renewIcon from './renew.svg';
+import imgreturnleft from './return.svg';
 
-  // Lodash, currently included via a script, is required for this line to work
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
+const headingimg = document.getElementById('heading');
+const myIcon = new Image();
+myIcon.src = renewIcon;
+headingimg.appendChild(myIcon);
 
-  element.appendChild(myIcon);
+const formimg = document.getElementById('form');
+const formIcon = new Image();
+formIcon.src = imgreturnleft;
+formimg.appendChild(formIcon);
 
-  return element;
-}
+const data = [
+  {
+    description: 'finish the project',
+    completed: 'true',
+    index: '1',
+  },
+  {
+    description: 'take rest',
+    completed: 'false',
+    index: '2',
+  },
+  {
+    description: 'enjoy life to the fullest',
+    completed: 'false',
+    index: '3',
+  },
+];
 
-document.body.appendChild(component());
+data.forEach((task) => {
+  const todolist = document.getElementById('todo-list');
+  const li = document.createElement('li');
+  li.className = 'task';
+  li.innerHTML = `
+  <label class="${task.index}">
+      <input type="checkbox" class="${task.index}">
+      <p>${task.description}</p>
+  </label>
+  <i class="fa fa-ellipsis-v moreVert"></i>
+  `;
+  todolist.appendChild(li);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
-      template: './src/index.html'
+      title: 'To Do List',
+      template: './src/index.html',
     }),
   ],
   output: {
