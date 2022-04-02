7ff15240f94cda56c6f10890781c3267e60f1294
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-04-02 16:38:43 +0430

    Implement add and remove for todoList

--- src/index.js
+++ src/index.js
@@ -1,45 +1,17 @@
 import './style.css';
-import renewIcon from './renew.svg';
-import imgreturnleft from './return.svg';
+import {
+  getStore, add, saveEdit, display, getIsEditing,
+} from './Add&Remove.js';
 
-const headingimg = document.getElementById('heading');
-const myIcon = new Image();
-myIcon.src = renewIcon;
-headingimg.appendChild(myIcon);
-
-const formimg = document.getElementById('form');
-const formIcon = new Image();
-formIcon.src = imgreturnleft;
-formimg.appendChild(formIcon);
-
-const data = [
-  {
-    description: 'finish the project',
-    completed: 'true',
-    index: '1',
-  },
-  {
-    description: 'take rest',
-    completed: 'false',
-    index: '2',
-  },
-  {
-    description: 'enjoy life to the fullest',
-    completed: 'false',
-    index: '3',
-  },
-];
-
-data.forEach((task) => {
-  const todolist = document.getElementById('todo-list');
-  const li = document.createElement('li');
-  li.className = 'task';
-  li.innerHTML = `
-  <label class="${task.index}">
-      <input type="checkbox" class="${task.index}">
-      <p>${task.description}</p>
-  </label>
-  <i class="fa fa-ellipsis-v moreVert"></i>
-  `;
-  todolist.appendChild(li);
+window.onload = () => {
+  getStore();
+  display();
+};
+const detail = document.querySelector('.taskList');
+detail.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    if (!getIsEditing())add();
+    else saveEdit();
+  }
 });
